b276994fb9cf9ba579e5dcefcbe058a82022bae4
KeitoIsurugi <ja224kg@gmail.com>
2024-03-03 21:55:36 +0900

    更新

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,10 @@ export default function Home() {
   return (
     <main>
       <h1>トップページ</h1>
-      <Link href="/todos">TODO APP</Link><br />
+      <Link href="/todos">TODO APP</Link>
+      <br />
+      <a href="https://nextjs.org/" target='_blank'>Next.js</a>
+      <br />
       <a href="https://nextjs.org/" target='_blank'>Next.js</a>
     </main>
   );
